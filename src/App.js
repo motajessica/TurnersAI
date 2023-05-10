867b80f98ad980d38c5e2e0b87d9592122c499a3
motajessica <motajessicaa@gmail.com>
2023-05-10 12:38:00 +1200

    Add message when the image quality is low

--- src/App.js
+++ src/App.js
@@ -6,38 +6,37 @@ import Hero from "./components/Hero.js";
 import UploadImage from "./components/UploadImage.js";
 import Chatbot from "./components/Chatbot.js";
 import { carDb } from './db/carDb.js';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 
 import "./App.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-
 const App = () => {
   const [cars, setCars] = useState(carDb);
+  const [error, setError] = useState("");
   const handleCategoryChange = function(category) {
     const filteredCars = carDb.filter(car => car.category === category)
     setCars(filteredCars)
   }
-
   function handleInvalidImage(message) {
-
+    setCars([]);
+    setError(message); 
   }
-
   return (
     <div>
       <Header />
       <Hero />  
       <div className="">
         <Search />
-        <UploadImage onCategoryChange={handleCategoryChange} onInvalidImage={handleInvalidImage}/>
+          <UploadImage onCategoryChange={handleCategoryChange} onInvalidImage={handleInvalidImage}/>
       </div>
-
       <div className="container">
         <div className="d-flex flex-wrap justify-content-center">
           {cars.map(function (car) {
             return <Card car={car} />;
           })}
+          {error && <h2>{error}</h2>}
         </div>
       </div>
         <Chatbot />
--- src/components/UploadImage.js
+++ src/components/UploadImage.js
@@ -2,12 +2,82 @@ import React from 'react';
 import { useState } from 'react';
 import { PREDICTION_KEY, IMAGE_PREDICTION_ENDPOINT } from '../config.js';
 
-function getProbability(json) {
-  const predictions = json.predictions
-  const maxProbability = Math.max(...predictions.map(prediction => prediction.probability))
-  const prediction = predictions.find(prediction => prediction.probability === maxProbability) 
+// function getProbability(json) {
+//   const predictions = json.predictions
+//   const maxProbability = Math.max(...predictions.map(prediction => prediction.probability))
+//   const prediction = predictions.find(prediction => prediction.probability === maxProbability) 
 
-  return prediction.tagName
+//   return prediction.tagName
+// }
+
+// function UploadImage(props) {
+
+//   const [imageFile, setImageFile] = useState(null);
+//   const handleFileChange = (event) => {
+//     const selectedFile = event.target.files[0];
+//     setImageFile(selectedFile);
+//   }
+//   const handleSubmit = async (event) => {
+//     event.preventDefault();
+//     if (!imageFile) {
+//       return;
+//     }
+//     const formData = new FormData();
+//     formData.append('image', imageFile);
+//     const response = await fetch(
+//       IMAGE_PREDICTION_ENDPOINT, 
+//       {
+//         method: 'POST',
+//         body: formData,
+//         headers: {
+//           'Prediction-Key': PREDICTION_KEY
+//         }
+//       }
+//     );
+//     const json = await response.json();
+//     if(response.ok) {
+//       props.onCategoryChange(getProbability(json))
+//     } else {
+//       props.onInvalidImage("Your image didn't return any results. Try another one.")
+//     }
+//     console.log(json);
+//     }
+
+
+//   //  .then(response => {
+//   //       console.log("ok")
+
+//   //     })
+//   //     .catch(error => {
+//   //       console.log("error")
+    
+//   //     });
+//   // }
+
+//   return (
+//     <div className='container'>
+//     <form onSubmit={handleSubmit}>
+//      <label>
+//         Upload Image:
+//         <input type="file" onChange={handleFileChange} />
+//       </label>
+//       <button type="submit">Submit</button>
+//     </form>
+//     </div>
+//   );
+// }
+
+// export default UploadImage;
+
+function getProbability(json) {
+  const predictions = json.predictions;
+  const maxProbability = Math.max(...predictions.map(prediction => prediction.probability));
+  const prediction = predictions.find(prediction => prediction.probability === maxProbability);
+  if (maxProbability >= 0.75) {
+    return prediction.tagName;
+  } else {
+    return null;
+  }
 }
 
 function UploadImage(props) {
@@ -35,24 +105,14 @@ function UploadImage(props) {
       }
     );
     const json = await response.json();
-    if(response.ok) {
-      props.onCategoryChange(getProbability(json))
+    const prediction = getProbability(json);
+    if (prediction !== null) {
+      props.onCategoryChange(prediction);
     } else {
-      props.onInvalidImage("Your image didn't return any results. Try another one.")
+      props.onInvalidImage("Please upload another image as this one is poor in quality");
     }
     console.log(json);
     }
-
-
-  //  .then(response => {
-  //       console.log("ok")
-
-  //     })
-  //     .catch(error => {
-  //       console.log("error")
-    
-  //     });
-  // }
 
   return (
     <div className='container'>
@@ -66,5 +126,4 @@ function UploadImage(props) {
     </div>
   );
 }
-
 export default UploadImage;
